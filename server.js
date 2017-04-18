a225791207713bca4370cb8f0afc82aa8670be99
Sebastian Petak <sebastianpetak@gmail.com>
2017-04-18 16:17:17 -0500

    Addfavicon Site lacks a favicon. Install serve-favicon serve the favicon.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const favicon = require('serve-favicon');
 const port = process.env.PORT || 8001;
 const path = require('path');
 
@@ -9,9 +10,13 @@ const routes = require('./app/routes.js');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// setup static files ========================================================
+// setup static files =========================================================
 app.use(express.static(path.join(__dirname, 'views/styles')));
 app.use(express.static(path.join(__dirname, 'views/assets')));
+
+// serve Favicon ==============================================================
+app.use(favicon(path.join(__dirname, 'views', 'assets', 'coffee-favicon',
+													'favicon.ico')));
 
 // Launch =====================================================================
 routes(app);
